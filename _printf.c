160083e6c9dfa0c67a3608abb15640030f5e2c95
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-05-31 00:31:37 +0300

    printf surgery lines

--- _printf.c
+++ _printf.c
@@ -1,60 +1,48 @@
 #include "main.h"
-
+/**
+ *_printf - a function that immitates the standard printf
+ *@format: the input string
+ *Return: the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list all_args;
-	char *buffer;
+	char *buffer = malloc(sizeof(char) * 1024);
 	unsigned int i = 0, length = 0, buffer_index = 0;
 	int (*func)(va_list, char *, unsigned int);
 
 	va_start(all_args, format);
-	buffer = malloc(sizeof(char) * 1024);
-
 	if (!buffer || !format)
 		return (-1);
-
 	if (!*format)
 		return (0);
-
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] != '%')
 			buffer_index = add_buff(buffer, format[i], buffer_index), length++;
 		else if (!*(format + i + 1))
 		{
-			print_buffer(buffer, buffer_index);
-			free(buffer);
-			va_end(all_args);
+			print_buffer(buffer, buffer_index), free(buffer), va_end(all_args);
 			return (-1);
 		}
 		else
 		{
 			func = getfunc(format, i + 1);
-
 			if (!func)
 			{
 				if (*(format + i + 1) == ' ' && !format[i + 2])
 				{
-					free(buffer);
-					va_end(all_args);
+					free(buffer), va_end(all_args);
 					return (-1);
-				}
-				buffer_index = add_buff(buffer, format[i], buffer_index);
-				length++;
+				} buffer_index = add_buff(buffer, format[i], buffer_index), length++;
 			}
 			else
-			{
-				i += skip(format, i + 1);
-				length += func(all_args, buffer, buffer_index);
-
-			}
+				i += skip(format, i + 1), length += func(all_args, buffer, buffer_index);
 		}
 
 		buffer_index = length % 1024;
 	}
 
-	print_buffer(buffer, buffer_index);
-	free(buffer);
-	va_end(all_args);
+	print_buffer(buffer, buffer_index), free(buffer), va_end(all_args);
 	return (length);
 }
