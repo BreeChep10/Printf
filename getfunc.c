48e208930240f371fa674ff1cad71e62a1724f82
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-05-31 01:30:25 +0300

    Betty fix

--- getfunc.c
+++ getfunc.c
@@ -1,37 +1,43 @@
 #include "main.h"
+/**
+ * getfunc - Function that points to fetch specifiers.
+ * @format: The format entered.
+ * @index: The position of format.
+ *
+ * Return: Function pointer.
+ */
 
 int (*getfunc(const char *format, int index))(va_list, char *, unsigned int)
 {
 	spec_t specifiers[] = {
-		{"d", print_integer}, {"i", print_integer}, {"c", print_char}, {"s", str_print},
-		{"%", prct}, {"u", print_uint}, {"o", print_uoctal}, {"X", print_upper_hex},
-		{"x", print_lower_hex}, {"b", binary_conversion}, {"R", print_rot13}, {"p", print_memory_address},
-		{"r", print_reverse}, {"hd", shorty_integer}, {"hi", shorty_integer}, {"ld", longy_integer},
-		{"li", longy_integer}, {"S", print_codehex}, {"#x", print_0xhex}, {"#X", print_0Xhex},
-		{"#o", octal_print}, {" d", _print_int}, {" d", _print_int}, {"hx", short_hex},
-                {"hX", short_heX}, {"hu", short_uint}, {"ho", short_octal}, {"lu", louint},
-                {"lo", louoct}, {NULL, NULL}
+		{"d", print_integer}, {"i", print_integer}, {"c", print_char},
+		{"s", str_print}, {"%", prct}, {"u", print_uint},
+		{"o", print_uoctal}, {"X", print_upper_hex}, {"x", print_lower_hex},
+		{"b", binary_conversion}, {"R", print_rot13}, {"p", print_memory_address},
+		{"r", print_reverse}, {"hd", shorty_integer}, {"hi", shorty_integer},
+		{"ld", longy_integer}, {"li", longy_integer}, {"S", print_codehex},
+		{"#x", print_0xhex}, {"#X", print_0Xhex}, {"#o", octal_print},
+		{" d", _print_int}, {" d", _print_int}, {"hx", short_hex},
+		{"hX", short_heX}, {"hu", short_uint}, {"ho", short_octal},
+		{"lu", louint}, {"lo", louoct}, {NULL, NULL}
 	};
-	
+
 	int i = 0, j = 0;
 	int reset = index;
-	
+
 	while (specifiers[i].data != NULL)
 	{
 		j = 0;
 		index = reset;
-		
 		while (format[index] && format[index] == specifiers[i].data[j])
 		{
 			index++;
 			j++;
-			
+
 			if (specifiers[i].data[j] == '\0')
-				return specifiers[i].f;
+				return (specifiers[i].f);
 		}
-		
 		i++;
 	}
-	
-	return NULL;
+	return (NULL);
 }
