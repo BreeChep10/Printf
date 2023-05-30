ad392aa6ba67327a2d84783d704ad5c24f33961a
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-05-30 21:30:27 +0300

    handling long flag

--- skip.c
+++ skip.c
@@ -7,8 +7,8 @@ int skip(const char *format, int index)
 		{"d", print_integer}, {"i", print_integer}, {"c", print_char}, {"s", str_print},
 		{"%", percent_print}, {"u", print_uint}, {"o", print_uoctal}, {"X", print_upper_hex},
 		{"x", print_lower_hex}, {"b", binary_conversion}, {"R", print_rot13}, {"p", print_memory_address},
-		{"r", print_reverse}, {"hd", shorty_integer}, {"hi", shorty_integer},/* {"ld", longy_integer},
-		{"li", longy_integer}, {"#x", print_0xhex},*/ {"S", print_codehex},/* {"#X", print_0Xhex},
+		{"r", print_reverse}, {"hd", shorty_integer}, {"hi", shorty_integer}, {"ld", longy_integer},
+		{"li", longy_integer},{"S", print_codehex}, /*{"#x", print_0xhex}, {"#X", print_0Xhex},
 		{"#o", octal_print}, {" d", _print_int}, {" d", _print_int},*/ {NULL, NULL}
 	};
 
