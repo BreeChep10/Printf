2c8ebcec54a90b17ea151bdd07da5aea7787c233
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-06-02 21:37:18 +0300

    fixing a stubborn hiding bug

--- print_octal.c
+++ print_octal.c
@@ -13,7 +13,7 @@ int print_octal(va_list all_args, char *buffer, unsigned int buffer_size)
 
 	binary_array = to_binary(binary_array, num, 32);
 	octal_array = binary_to_octal(binary_array, octal_array, 11);
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < 11; i++)
 	{
 		if (octal_array[i] != '0')
                         check = 1;
